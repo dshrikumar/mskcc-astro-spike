37761c6715edabe354c2888bf07a41c87804e69e
Shri Kumar <shrikumar.d@icliniq.com>
2023-03-31 16:46:58 +0530

    Make use of cloudflare adapter instead of vercel | Shri Kumar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-// import vercel from "@astrojs/vercel/serverless";
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  // adapter: vercel()
-  adapter: cloudflare({ mode: "directory" })
+  adapter: cloudflare()
 });
